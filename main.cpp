b5cae69c647ba6b9a2b1bd5646004c9a68395663
Montanaro02 <100145766+Montanaro02@users.noreply.github.com>
2022-03-30 14:46:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,25 @@
 using namespace std;
 
 int main() {
-   return 0;
+    float a=0, b=0, triangolo=0, quadrato=0, rettangolo=0;
+    cin >> a>> b;
+    int opzione=0;
+    cin >> opzione;
+    switch(opzione) {
+        case 0:
+            triangolo=a*b/2;
+            cout << triangolo;
+            break;
+        case 1:
+            quadrato=a*a;
+            cout << quadrato;
+            break;
+        case 2:
+            rettangolo=a*b;
+            cout <<rettangolo;
+            break;
+        default:
+            cout << "opzione non valida" <<endl;
+    }
+    return 0;
 }
